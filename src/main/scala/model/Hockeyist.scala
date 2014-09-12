4b98c8380e572ee27c4c98c20ee62ed8397e407f
Serge Ivanov <lutvuk@gmail.com>
2014-09-12 19:49:07 +0400

    Hockeyist style refactoring. Readme fix

--- src/main/scala/model/Hockeyist.scala
+++ src/main/scala/model/Hockeyist.scala
@@ -2,102 +2,68 @@ package model
 
 /**
  * Класс, определяющий хоккеиста. Содержит также все свойства юнита.
+ * @param id Возвращает уникальный идентификатор объекта.
+ * @param playerId Возвращает идентификатор игрока, в команду которого входит хоккеист.
+ * @param teammateIndex Возвращает 0-индексированный номер хоккеиста в команде.
+ * @param mass Возвращает массу объекта в единицах массы.
+ * @param radius Возвращает радиус объекта.
+ * @param x Возвращает X-координату центра объекта. Ось абсцисс направлена слева направо.
+ * @param y Возвращает Y-координату центра объекта. Ось ординат направлена свеху вниз.
+ * @param speedX Возвращает X-составляющую скорости объекта. Ось абсцисс направлена слева направо.
+ * @param speedY Возвращает Y-составляющую скорости объекта. Ось ординат направлена свеху вниз.
+ * @param angle Возвращает угол поворота объекта в радианах. Нулевой угол соответствует направлению оси абсцисс.
+ *              Положительные значения соответствуют повороту по часовой стрелке.
+ * @param angularSpeed Возвращает скорость вращения объекта.
+ *                     Положительные значения соответствуют вращению по часовой стрелке.
+ * @param teammate Возвращает `true`, если и только если данный хоккеист входит в команду вашего игрока.
+ * @param hokeyistType Возвращает тип хоккеиста.
+ * @param strength Возвращает значение атрибута сила.
+ * @param endurance Возвращает значение атрибута стойкость.
+ * @param dexterity Возвращает значение атрибута ловкость.
+ * @param agility Возвращает значение атрибута подвижность.
+ * @param stamina Возвращает текущее значение выносливости.
+ * @param state Возвращает состояние хоккеиста.
+ * @param originalPositionIndex Возвращает индекс исходной позиции хоккеиста или `1` для вратаря или хоккеиста,
+ *                              отдыхающего за пределами игрового поля. На эту позицию хоккеист будет помещён
+ *                              при разыгрывании шайбы.При выполнении действия замена [[model.ActionType.Substitute]]
+ *                              индексы исходных позиций хоккеистов, участвующих в замене, меняются местами.
+ * @param remainingKnockdownTicks Возвращает количество тиков, по прошествии которого хоккеист восстановится после падения,
+ *                                или `0`, если хоккеист не сбит с ног.
+ * @param remainingCooldownTicks Возвращает количество тиков, по прошествии которого хоккеист сможет совершить какое-либо
+ *                               действие ([[model.Move#setAction]]), или `0`, если хоккеист может совершить
+ *                               действие в данный тик.
+ * @param swingTicks Для хоккеиста, находящегося в состоянии замаха ([[model.HockeyistState.Swinging]]),
+ *                   возвращает количество тиков, прошедших от начала замаха. В противном случае возвращает `0`.
+ * @param lastAction Возвращает последнее действие ([[model.Move#setAction]]), совершённое хоккеистом,
+ *                   или [[model.ActionType.None]] в случае, если хоккеист ещё не совершил ни одного действия.
+ * @param lastActionTick Возвращает номер тика, в который хоккеист совершил своё последние действие ([[model.Move#setAction]]),
+ *                       или `None` в случае, если хоккеист ещё не совершил ни одного действия.
  */
-class Hockeyist(id: Long, playerId: Long, teammateIndex: Int, mass: Double,
-                radius: Double, x: Double, y: Double, speedX: Double, speedY: Double,
-                angle: Double, angularSpeed: Double, teammate: Boolean, hokeyistType: Option[HockeyistType],
-                strength: Int, endurance: Int, dexterity: Int, agility: Int, stamina: Double,
-                state: Option[HockeyistState], originalPositionIndex: Int, remainingKnockdownTicks: Int,
-                remainingCooldownTicks: Int, swingTicks: Int, lastAction: Option[ActionType],
-                lastActionTick: Option[Integer])
-  extends Unit(id, mass, radius, x, y, speedX, speedY, angle, angularSpeed) {
-
-  /**
-   * @return Возвращает идентификатор игрока, в команду которого входит хоккеист.
-   */
-  def getPlayerId: Long = playerId
-
-  /**
-   * @return Возвращает 0-индексированный номер хоккеиста в команде.
-   */
-  def getTeammateIndex: Int = teammateIndex
-
-  /**
-   * @return Возвращает `true`, если и только если данный хоккеист входит в команду вашего игрока.
-   */
-  def isTeammate: Boolean = teammate
-
-  /**
-   * @return Возвращает тип хоккеиста.
-   */
-  def getType: Option[HockeyistType] = hokeyistType
-
-  /**
-   * @return Возвращает значение атрибута сила.
-   */
-  def getStrength: Int = strength
-
-  /**
-   * @return Возвращает значение атрибута стойкость.
-   */
-  def getEndurance: Int = endurance
-
-  /**
-   * @return Возвращает значение атрибута ловкость.
-   */
-  def getDexterity: Int = dexterity
-
-  /**
-   * @return Возвращает значение атрибута подвижность.
-   */
-  def getAgility: Int = agility
-
-  /**
-   * @return Возвращает текущее значение выносливости.
-   */
-  def getStamina: Double = stamina
-
-  /**
-   * @return Возвращает состояние хоккеиста.
-   */
-  def getState: Option[HockeyistState] = state
-
-  /**
-   * @return Возвращает индекс исходной позиции хоккеиста или `1` для вратаря или хоккеиста,
-   *         отдыхающего за пределами игрового поля. На эту позицию хоккеист будет помещён при разыгрывании шайбы.
-   *         При выполнении действия замена [[model.ActionType.Substitute]] индексы исходных позиций хоккеистов,
-   *         участвующих в замене, меняются местами.
-   */
-  def getOriginalPositionIndex: Int = originalPositionIndex
-
-  /**
-   * @return Возвращает количество тиков, по прошествии которого хоккеист восстановится после падения,
-   *         или `0`, если хоккеист не сбит с ног.
-   */
-  def getRemainingKnockdownTicks: Int = remainingKnockdownTicks
-
-  /**
-   * @return Возвращает количество тиков, по прошествии которого хоккеист сможет совершить какое-либо
-   *         действие ([[model.Move#setAction]]), или `0`, если хоккеист может совершить действие в данный тик.
-   */
-  def getRemainingCooldownTicks: Int = remainingCooldownTicks
-
-  /**
-   * @return Для хоккеиста, находящегося в состоянии замаха ([[model.HockeyistState.Swinging]]),
-   *         возвращает количество тиков, прошедших от начала замаха. В противном случае возвращает `0`.
-   */
-  def getSwingTicks: Int = swingTicks
-
-  /**
-   * @return Возвращает последнее действие ([[model.Move#setAction]]), совершённое хоккеистом,
-   *         или [[model.ActionType.None]] в случае, если хоккеист ещё не совершил ни одного действия.
-   */
-  def getLastAction: Option[ActionType] = lastAction
-
-  /**
-   * @return Возвращает номер тика, в который хоккеист совершил своё последние действие ([[model.Move#setAction]]),
-   *         или `None` в случае, если хоккеист ещё не совершил ни одного действия.
-   */
-  def getLastActionTick: Option[Integer] = lastActionTick
-}
+class Hockeyist(id: Long,
+                val playerId: Long,
+                val teammateIndex: Int,
+                mass: Double,
+                radius: Double,
+                x: Double,
+                y: Double,
+                speedX: Double,
+                speedY: Double,
+                angle: Double,
+                angularSpeed: Double,
+                val teammate: Boolean,
+                val hokeyistType: Option[HockeyistType],
+                val strength: Int,
+                val endurance: Int,
+                val dexterity: Int,
+                val agility: Int,
+                val stamina: Double,
+                val state: Option[HockeyistState],
+                val originalPositionIndex: Int,
+                val remainingKnockdownTicks: Int,
+                val remainingCooldownTicks: Int,
+                val swingTicks: Int,
+                val lastAction: Option[ActionType],
+                val lastActionTick: Option[Integer])
+  extends Unit(id, mass, radius, x, y, speedX, speedY, angle, angularSpeed)
+{}
 
