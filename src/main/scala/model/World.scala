0436ca1110efa083940a2c8b9eea1fd04419e645
Alexander Myltsev <alexander.myltsev@phystech.edu>
2014-09-12 17:47:28 +0400

    Refactor World class

--- src/main/scala/model/World.scala
+++ src/main/scala/model/World.scala
@@ -2,56 +2,32 @@ package model
 
 /**
  * Этот класс описывает игровой мир. Содержит также описания всех игроков и игровых объектов (<<юнитов>>).
+ * @param tick Возвращает номер текущего тика.
+ * @param tickCount Возвращает базовую длительность игры в тиках.
+ *                  Реальная длительность может отличаться от этого значения в большую сторону.
+ * @param width Возвращает ширину мира.
+ * @param height Возвращает высоту мира.
+ * @param players Возвращает список игроков (в случайном порядке).
+ *                После каждого тика объекты, задающие игроков, пересоздаются.
+ * @param hockeyists Возвращает список хоккеистов (в случайном порядке), включая вратарей и хоккеиста стратегии,
+ *                   вызвавшей этот метод. После каждого тика объекты, задающие хоккеистов, пересоздаются.
+ * @param puck Возвращает шайбу.
  */
-class World(tick: Int, tickCount: Int, width: Double, height: Double,
-            players: Vector[Player], hockeyists: Vector[Option[Hockeyist]], puck: Option[Puck])
-{
-  /**
-   * @return Возвращает номер текущего тика.
-   */
-  def getTick: Int = tick
-
-  /**
-   * @return Возвращает базовую длительность игры в тиках.
-   *         Реальная длительность может отличаться от этого значения в большую сторону.
-   */
-  def getTickCount: Int = tickCount
-
-  /**
-   * @return Возвращает ширину мира.
-   */
-  def getWidth: Double = width
-
-  /**
-   * @return Возвращает высоту мира.
-   */
-  def getHeight: Double = height
-
-  /**
-   * @return Возвращает список игроков (в случайном порядке).
-   *         После каждого тика объекты, задающие игроков, пересоздаются.
-   */
-  def getPlayers: Vector[Player] = players
-
-  /**
-   * @return Возвращает список хоккеистов (в случайном порядке), включая вратарей и хоккеиста стратегии,
-   *         вызвавшей этот метод. После каждого тика объекты, задающие хоккеистов, пересоздаются.
-   */
-  def getHockeyists: Vector[Option[Hockeyist]] = hockeyists
-
-  /**
-   * @return Возвращает шайбу.
-   */
-  def getPuck: Option[Puck] = puck
+class World(val tick: Int, 
+            val tickCount: Int, 
+            val width: Double, 
+            val height: Double,
+            val players: Vector[Option[Player]], 
+            val hockeyists: Vector[Option[Hockeyist]], 
+            val puck: Option[Puck]) {
 
   /**
    * @return Возвращает вашего игрока.
    */
-  def getMyPlayer: Option[Player] = players.find(_.isMe)
+  lazy val myPlayer: Option[Player] = players.flatten.find(_.isMe)
 
   /**
    * @return Возвращает игрока, соревнующегося с вами.
    */
-  def getOpponentPlayer: Option[Player] = players.filterNot(_.isMe).headOption
+  lazy val opponentPlayer: Option[Player] = players.flatten.find(!_.isMe)
 }
-
